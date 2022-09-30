ab5db69c4654d4eec189b5fbb941bf2f5bb6a9cc
Vishakha-Jangam <101566433+Vishakha-Jangam@users.noreply.github.com>
2022-09-30 22:39:13 +0530

    CurrentUserSession entity added

--- PaymentWallet/src/main/java/com/masai/entity/CurrentUserSession.java
+++ PaymentWallet/src/main/java/com/masai/entity/CurrentUserSession.java
@@ -1,5 +1,58 @@
 package com.masai.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
 public class CurrentUserSession {
 
+	@Id
+	@Size(min=10,max=10)
+	@Pattern(regexp="([A-Z]{5}[0-9]{4}[A-Z]{1})", message = "Please,enter valid pancard number")
+	private String pancard;
+    private LocalDateTime localDateTime;
+    @Column(unique = true)
+    private String uuid;
+
+	public CurrentUserSession() {
+		super();
+	}
+	public CurrentUserSession(String pancard, LocalDateTime localDateTime, String uuid, long mobileNum) {
+		super();
+		this.pancard = pancard;
+		this.localDateTime = localDateTime;
+		this.uuid = uuid;
+	
+	}
+	public String getPancard() {
+		return pancard;
+	}
+	public void setPancard(String pancard) {
+		this.pancard = pancard;
+	}
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+	public void setLocalDateTime(LocalDateTime localDateTime) {
+		this.localDateTime = localDateTime;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	@Override
+	public String toString() {
+		return "CurrentUserSession [pancard=" + pancard + ", localDateTime=" + localDateTime + ", uuid=" + uuid + "]";
+	}
+	
+	
+    
+    
 }
